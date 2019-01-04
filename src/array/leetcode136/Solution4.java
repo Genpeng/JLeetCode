7c3bb9e0920baa64bdeb55af828acb882db65750
Genpeng <xgp1227@gmail.com>
2019-01-04 09:46:26 +0800

    [Add] The solutions of no. 136 problem in the LeetCode

---
+++ src/array/leetcode136/Solution4.java
@@ -0,0 +1,51 @@
+package array.leetcode136;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This is the solution of No. 136 problem in the LeetCode,
+ * the website of the problem is as follow:
+ * https://leetcode.com/problems/single-number/
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *
+ * Example 1:
+ * Input: [2,2,1]
+ * Output: 1
+ *
+ * Example 2:
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ * ==========================================================================================================
+ *
+ * @author  StrongXGP (xgp1227@gmail.com)
+ * @date    2019/01/04
+ */
+public class Solution4 {
+    /**
+     * 解法四：位运算
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     *
+     * @param nums int[], the input array of integers
+     * @return int, the only integer which appears once
+     */
+    public int singleNumber(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1};
+        System.out.println((new Solution4()).singleNumber(nums));
+    }
+}
