a8af237f18e3b2f463c244cde6974d5cb286340e
“Genpeng” <xgp1227@gmai.com>
2019-03-19 06:02:39 +0800

    [Add] the solution of no. 283 problem in the LeetCode

--- src/lc283/Solution1.java
+++ src/lc283/Solution1.java
@@ -1,11 +1,25 @@
-package array.leetcode283;
+package lc283;
 
 import java.util.Arrays;
 
 /**
- * This is the solution of No. 283 problem in the LeetCode,
+ * This is the solution of No. xxx problem in the LeetCode,
  * the website of the problem is as follow:
  * https://leetcode.com/problems/move-zeroes/
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * Given an array nums, write a function to move all 0's to the end of it
+ * while maintaining the relative order of the non-zero elements.
+ *
+ * Example:
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ *
+ * Note:
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ * ==========================================================================================================
  *
  * @author  StrongXGP (xgp1227@gmail.com)
  * @date    2018/12/07
