55fa76d199b4055df5b4cb4ca315ff2105db906a
“Genpeng” <xgp1227@gmai.com>
2019-05-15 09:59:34 +0800

    Add the solution of no. 144 problem in the LeetCode

---
+++ src/lc0144_binary_tree_preorder_traversal/Solution2.java
@@ -0,0 +1,64 @@
+package lc0144_binary_tree_preorder_traversal;
+
+import entity.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * This is the solution of No. 144 problem in the LeetCode,
+ * the website of the problem is as follow:
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ *
+ * Example:
+ *
+ * Input: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * Output: [1,2,3]
+ *
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * ==========================================================================================================
+ *
+ * @author  StrongXGP (xgp1227@gmail.com)
+ * @date    2019/05/15
+ */
+public class Solution2 {
+    /**
+     * 方法二：迭代
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     *
+     * @param root TreeNode, the root of BST
+     * @return List<Integer>, the preorder traversal of BST
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return res;
+    }
+}
