3e1b78bfa30fcc31d37d29a2e875b3739632892b
“Genpeng” <xgp1227@gmai.com>
2019-05-16 13:29:58 +0800

    Add the solution of no. 145 problem in the LeetCode

---
+++ src/lc0145_binary_tree_postorder_traversal/Solution2.java
@@ -0,0 +1,59 @@
+package lc0145_binary_tree_postorder_traversal;
+
+import entity.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * This is the solution of No. 145 problem in the LeetCode,
+ * the website of the problem is as follow:
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ *
+ * Example:
+ *
+ * Input: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * Output: [3,2,1]
+ *
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * ==========================================================================================================
+ *
+ * @author  StrongXGP (xgp1227@gmail.com)
+ * @date    2019/05/15
+ */
+public class Solution2 {
+    /**
+     * 解法二：迭代
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     *
+     * @param root TreeNode, the root of binary tree
+     * @return List<Integer>, the postorder traversal of binary tree
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            if (curr != null) {
+                res.addFirst(curr.val);
+                stack.push(curr);
+                curr = curr.right;
+            } else {
+                curr = stack.pop().left;
+            }
+        }
+        return res;
+    }
+}
