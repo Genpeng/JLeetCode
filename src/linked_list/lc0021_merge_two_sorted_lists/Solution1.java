16952e719f7d17e482ec2bce243d7af81e4fc883
“Genpeng” <xgp1227@gmai.com>
2019-05-27 14:28:34 +0800

    Modify the solution of no. 21 problem in the LeetCode

--- src/linked_list/lc0021_merge_two_sorted_lists/Solution1.java
+++ src/linked_list/lc0021_merge_two_sorted_lists/Solution1.java
@@ -32,18 +32,19 @@ public class Solution1 {
      */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(-1);
-        ListNode curr = dummyHead;
+        ListNode prev = dummyHead;
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
-                curr.next = l1;
+                prev.next = l1;
                 l1 = l1.next;
             } else {
-                curr.next = l2;
+                prev.next = l2;
                 l2 = l2.next;
             }
-            curr = curr.next;
+            // update to next iteration
+            prev = prev.next;
         }
-        curr.next = l1 == null ? l2 : l1;
+        prev.next = l1 == null ? l2 : l1;
         return dummyHead.next;
     }
 
