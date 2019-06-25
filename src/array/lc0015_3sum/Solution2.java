142ade94c1c89abd83ec0fe36e2941c07e7a0704
“Genpeng” <xgp1227@gmai.com>
2019-06-25 17:49:53 +0800

    Modify the solution of no. 15 problem in the LeetCode

--- src/array/lc0015_3sum/Solution2.java
+++ src/array/lc0015_3sum/Solution2.java
@@ -39,18 +39,17 @@ public class Solution2 {
      * @return List<List<Integer>>, all unique triplets in the array which gives the sum of zero
      */
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
         if (nums == null || nums.length < 3) {
-            return new LinkedList<>();
+            return res;
         }
 
         Arrays.sort(nums);
-        List<List<Integer>> res = new LinkedList<>();
-        int n = nums.length;
-        for (int i = 0; i < n - 2; ++i) {
+        for (int i = 0; i < nums.length - 2; ++i) {
             if (i > 0 && nums[i-1] == nums[i]) {
                 continue;
             }
-            int l = i + 1, r = n - 1;
+            int l = i + 1, r = nums.length - 1;
             while (l < r) {
                 int s = nums[i] + nums[l] + nums[r];
                 if (s < 0) {
@@ -58,13 +57,15 @@ public class Solution2 {
                 } else if (s > 0) {
                     --r;
                 } else {
-                    res.add(Arrays.asList(nums[i], nums[l++], nums[r++]));
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
                     while (l < r && nums[l] == nums[l+1]) {
                         ++l;
                     }
                     while (l < r && nums[r] == nums[r-1]) {
                         --r;
                     }
+                    ++l;
+                    --r;
                 }
             }
         }
