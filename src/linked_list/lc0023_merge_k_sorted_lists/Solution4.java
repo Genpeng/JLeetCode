b6ee83f515c768b01a420928efeb717fdf37bba9
“Genpeng” <xgp1227@gmai.com>
2019-08-14 16:18:57 +0800

    Add the solution of no. 23 problem in the LeetCode

---
+++ src/linked_list/lc0023_merge_k_sorted_lists/Solution4.java
@@ -0,0 +1,73 @@
+package linked_list.lc0023_merge_k_sorted_lists;
+
+import entity.ListNode;
+
+/**
+ * This is the solution of No. 23 problem in the LeetCode,
+ * the website of the problem is as follow:
+ * https://leetcode.com/problems/merge-k-sorted-lists/
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ *
+ * Example:
+ * Input:
+ * [
+ *   1->4->5,
+ *   1->3->4,
+ *   2->6
+ * ]
+ * Output: 1->1->2->3->4->4->5->6
+ * ==========================================================================================================
+ *
+ * @author  StrongXGP (xgp1227@gmail.com)
+ * @date    2019/07/04
+ */
+public class Solution4 {
+    /**
+     * 解法四：将问题分解成为合并两个链表（k-1次）
+     *
+     * 时间复杂度：O(N * k)
+     * 空间复杂度：O(1)
+     *
+     * @param lists ListNode[], k sorted linked lists
+     * @return ListNode, the new sorted linked list
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        ListNode head = lists[0];
+        for (int i = 1; i < lists.length; ++i) {
+            head = mergeTwoLists(head, lists[i]);
+        }
+        return head;
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode prev = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head1 = new ListNode(new int[] {1, 4, 5});
+        ListNode head2 = new ListNode(new int[] {1, 3, 4});
+        ListNode head3 = new ListNode(new int[] {2, 6});
+        ListNode[] lists = new ListNode[] {head1, head2, head3};
+        Solution4 solution = new Solution4();
+        System.out.println(solution.mergeKLists(lists));
+    }
+}
---
+++ src/linked_list/lc0023_merge_k_sorted_lists/Solution5.java
@@ -0,0 +1,87 @@
+package linked_list.lc0023_merge_k_sorted_lists;
+
+import entity.ListNode;
+
+/**
+ * This is the solution of No. 23 problem in the LeetCode,
+ * the website of the problem is as follow:
+ * https://leetcode.com/problems/merge-k-sorted-lists/
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ *
+ * Example:
+ * Input:
+ * [
+ *   1->4->5,
+ *   1->3->4,
+ *   2->6
+ * ]
+ * Output: 1->1->2->3->4->4->5->6
+ * ==========================================================================================================
+ *
+ * @author  StrongXGP (xgp1227@gmail.com)
+ * @date    2019/07/04
+ */
+public class Solution5 {
+    /**
+     * 解法五：Divide & Conquer (Top down)
+     *
+     * 时间复杂度：O(N * log(k))
+     * 空间复杂度：O(log(k))
+     *
+     * @param lists ListNode[], k sorted linked lists
+     * @return ListNode, the new sorted linked list
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return mergeKLists(lists, 0, lists.length - 1);
+    }
+
+    /**
+     * Merge k lists where the start index is `si`, and the end index is `ei`
+     *
+     * @param lists ListNode[], k lists
+     * @param si int, the start index
+     * @param ei int, the end index
+     * @return ListNode, the new sorted list
+     */
+    private ListNode mergeKLists(ListNode[] lists, int si, int ei) {
+        if (si == ei) {
+            return lists[si];
+        }
+        int mi = si + (ei - si) / 2;
+        ListNode l1 = mergeKLists(lists, si, mi);
+        ListNode l2 = mergeKLists(lists, mi + 1, ei);
+        return mergeTwoLists(l1, l2);
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode prev = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head1 = new ListNode(new int[] {1, 4, 5});
+        ListNode head2 = new ListNode(new int[] {1, 3, 4});
+        ListNode head3 = new ListNode(new int[] {2, 6});
+        ListNode[] lists = new ListNode[] {head1, head2, head3};
+        Solution5 solution = new Solution5();
+        System.out.println(solution.mergeKLists(lists));
+    }
+}
