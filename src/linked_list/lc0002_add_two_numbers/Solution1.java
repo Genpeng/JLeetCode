dc50de4cfb908046dda664ea30775acdde701afd
“Genpeng” <xgp1227@gmai.com>
2019-08-31 16:20:18 +0800

    Modify the solution of no. 2 problem in the LeetCode

--- src/linked_list/lc0002_add_two_numbers/Solution1.java
+++ src/linked_list/lc0002_add_two_numbers/Solution1.java
@@ -38,62 +38,22 @@ public class Solution1 {
         if (l1 == null || l2 == null) {
             return l1 == null ? l2 : l1;
         }
-
         ListNode dummyHead = new ListNode(-1);
-        ListNode curr = dummyHead;
+        ListNode tail = dummyHead;
         int carry = 0;
-        for (ListNode p1 = l1, p2 = l2; p1 != null || p2 != null; ) {
-            int sum = carry;
-            sum += p1 == null ? 0 : p1.val;
-            sum += p2 == null ? 0 : p2.val;
-
-            carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-
-            // update to next iteration
-            p1 = p1 == null ? p1 : p1.next;
-            p2 = p2 == null ? p2 : p2.next;
-            curr = curr.next;
-        }
-        if (carry > 0) {
-            curr.next = new ListNode(carry);
-        }
-        return dummyHead.next;
-    }
-
-    /**
-     * 解法一：非递归
-     * 时间复杂度：O(max(m, n))，其中m和n分别表示两个的长度
-     * 空间复杂度：O(max(m, n))，返回链表的长度最多为max(m, n)+1
-     *
-     * @param l1 ListNode, the head of one of linked list
-     * @param l2 ListNode, the head of the other linked list
-     * @return ListNode, result represents the sum of two linked lists
-     */
-    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
-        if (l1 == null || l2 == null) {
-            return l1 == null ? l2 : l1;
-        }
-
-        ListNode dummyHead = new ListNode(-1);
-        ListNode curr = dummyHead;
-        ListNode p1 = l1, p2 = l2;
-        int carry = 0;
-        while (p1 != null || p2 != null) {
+        while (l1 != null || l2 != null) {
             int s = carry;
-            s += p1 == null ? 0 : p1.val;
-            s += p2 == null ? 0 : p2.val;
-
-            carry = s / 10;
-            curr.next = new ListNode(s % 10);
-
+            s += l1 == null ? 0 : l1.val;
+            s += l2 == null ? 0 : l2.val;
+            tail.next = new ListNode(s % 10);
+            tail = tail.next;
             // update to next iteration
-            p1 = p1 == null ? p1 : p1.next;
-            p2 = p2 == null ? p2 : p2.next;
-            curr = curr.next;
+            carry = s / 10;
+            l1 = l1 == null ? l1 : l1.next;
+            l2 = l2 == null ? l2 : l2.next;
         }
         if (carry > 0) {
-            curr.next = new ListNode(carry);
+            tail.next = new ListNode(carry);
         }
         return dummyHead.next;
     }
@@ -101,6 +61,6 @@ public class Solution1 {
     public static void main(String[] args) {
         ListNode l1 = new ListNode(new int[] {2, 4, 3});
         ListNode l2 = new ListNode(new int[] {5, 6, 4});
-        System.out.println((new Solution1()).addTwoNumbers2(l1, l2));
+        System.out.println((new Solution1()).addTwoNumbers(l1, l2));
     }
 }
