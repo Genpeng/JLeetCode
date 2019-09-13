837dc53cfc0cba939bc670ad1dfcdd421ca12ea8
“Genpeng” <xgp1227@gmai.com>
2019-09-13 16:57:39 +0800

    Modify the solution of no. 61 problem in the LeetCode

--- src/linked_list/lc0061_rotate_list/Solution.java
+++ src/linked_list/lc0061_rotate_list/Solution.java
@@ -6,18 +6,18 @@ import entity.ListNode;
  * This is the solution of No. 61 problem in the LeetCode,
  * the website of the problem is as follow:
  * https://leetcode.com/problems/rotate-list/
- *
+ * <p>
  * The description of problem is as follow:
  * ==========================================================================================================
  * Given a linked list, rotate the list to the right by k places, where k is non-negative.
- *
+ * <p>
  * Example 1:
  * Input: 1->2->3->4->5->NULL, k = 2
  * Output: 4->5->1->2->3->NULL
  * Explanation:
  * rotate 1 steps to the right: 5->1->2->3->4->NULL
  * rotate 2 steps to the right: 4->5->1->2->3->NULL
- *
+ * <p>
  * Example 2:
  * Input: 0->1->2->NULL, k = 4
  * Output: 2->0->1->NULL
@@ -33,24 +33,21 @@ import entity.ListNode;
  */
 public class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null) {
+        if (head == null || head.next == null || k == 0) {
             return head;
         }
-
         // 求出链表的长度
-        int length = 1;
+        int n = 0;
         ListNode curr = head;
-        while (curr.next != null) {
+        while (curr != null) {
+            ++n;
             curr = curr.next;
-            ++length;
         }
-
         // 求出实际右移的次数
-        k = k % length;
+        k %= n;
         if (k == 0) {
             return head;
         }
-
         // 找到倒数第k+1个节点
         ListNode p1 = head, p2 = head;
         for (int i = 0; i < k; ++i) {
@@ -60,7 +57,6 @@ public class Solution {
             p1 = p1.next;
             p2 = p2.next;
         }
-
         // “右移”链表
         ListNode newHead = p1.next;
         p1.next = null;
@@ -68,61 +64,44 @@ public class Solution {
         return newHead;
     }
 
-    public ListNode rotateRight2(ListNode head, int k) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-
+    public ListNode rotateRightV2(ListNode head, int k) {
         // 求出链表的长度
+        int n = 0;
         ListNode curr = head;
-        int l = 0;
         while (curr != null) {
-            ++l;
+            ++n;
             curr = curr.next;
         }
-
         // 求出实际翻转的长度
-        k = k % l;
-        if (k == 0) {
+        k %= n; // the range of k is: [0, n-1]
+        // 翻转链表
+        return rotateNthFromEnd(head, k);
+    }
+
+    private ListNode rotateNthFromEnd(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
             return head;
         }
-
-        // 翻转链表
         ListNode p1 = head, p2 = head;
+        // 移动p2指针，使得p1和p2指针之间有k+1个节点
         for (int i = 0; i < k; ++i) {
             p2 = p2.next;
         }
+        // 同时移动p1和p2指针，使得p2指针处于链表的尾部
         while (p2.next != null) {
             p1 = p1.next;
             p2 = p2.next;
         }
+        // 翻转链表
         ListNode newHead = p1.next;
         p1.next = null;
         p2.next = head;
         return newHead;
     }
 
-     private ListNode rotateNthFromEnd(ListNode head, int k) {
-         ListNode p1 = head, p2 = head;
-         // 移动p2指针，使得p1和p2指针之间的距离为k+1
-         for (int i = 0; i < k; ++i) {
-             p2 = p2.next;
-         }
-         // 同时移动p1和p2指针，使得p2指针处于链表的尾部
-         while (p2.next != null) {
-             p1 = p1.next;
-             p2 = p2.next;
-         }
-         // 翻转链表
-         ListNode newHead = p1.next;
-         p1.next = null;
-         p2.next = head;
-         return newHead;
-     }
-
     public static void main(String[] args) {
-        ListNode head = new ListNode(new int[] {1, 2, 3, 4, 5});
+        ListNode head = new ListNode(new int[]{1, 2, 3, 4, 5});
         System.out.println(head);
-        System.out.println((new Solution()).rotateRight2(head, 2));
+        System.out.println((new Solution()).rotateRightV2(head, 2));
     }
 }
