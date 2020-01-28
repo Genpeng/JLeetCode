31366726420c7c176a536fa1eb8bbb5006965e7e
Genpeng Xu <xgp1227@gmai.com>
2020-01-28 23:06:39 +0800

    Modify the solution of no. 350 problem in the LeetCode

--- src/array/lc0350_intersection_of_two_arrays_ii/Solution2.java
+++ src/array/lc0350_intersection_of_two_arrays_ii/Solution2.java
@@ -26,10 +26,10 @@ import java.util.List;
  * The result can be in any order.
  *
  * Follow up:
- * What if the given array is already sorted? How would you optimize your algorithm?
- * What if nums1's size is small compared to nums2's size? Which algorithm is better?
- * What if elements of nums2 are stored on disk, and the memory is limited such that
- * you cannot load all elements into the memory at once?
+ * - What if the given array is already sorted? How would you optimize your algorithm?
+ * - What if nums1's size is small compared to nums2's size? Which algorithm is better?
+ * - What if elements of nums2 are stored on disk, and the memory is limited such that
+ *   you cannot load all elements into the memory at once?
  * ==========================================================================================================
  *
  * @author  StrongXGP (xgp1227@gmail.com)
