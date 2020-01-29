dcac6b843ece99e2ffe7475e5cb1122424c21ba4
Genpeng Xu <xgp1227@gmai.com>
2020-01-29 23:41:10 +0800

    Modify the solution of no. 747 problem in the LeetCode

--- src/array/lc0747_largest_number_at_least_twice_of_others/Solution.java
+++ src/array/lc0747_largest_number_at_least_twice_of_others/Solution.java
@@ -13,29 +13,18 @@ package array.lc0747_largest_number_at_least_twice_of_others;
  *
  * If it is, return the **index** of the largest element, otherwise return -1.
  *
- * **Example 1:**
- *
- * ```
+ * Example 1:
  * Input: nums = [3, 6, 1, 0]
  * Output: 1
  * Explanation: 6 is the largest integer, and for every other number in the array x,
  * 6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
- * ```
  *
- *
- *
- * **Example 2:**
- *
- * ```
+ * Example 2:
  * Input: nums = [1, 2, 3, 4]
  * Output: -1
  * Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
- * ```
- *
- *
- *
- * **Note:**
- *
+
+ * Note:
  * 1. `nums` will have a length in the range `[1, 50]`.
  * 2. Every `nums[i]` will be an integer in the range `[0, 99]`.
  * ==========================================================================================================
@@ -48,7 +37,6 @@ public class Solution {
         if (nums == null || nums.length == 0) {
             return -1;
         }
-
         // 1. 找出数组中最大值的索引
         int maxIndex = 0;
         for (int i = 1; i < nums.length; ++i) {
@@ -56,7 +44,6 @@ public class Solution {
                 maxIndex = i;
             }
         }
-
         // 2. 判断最大值是否是其他元素的两倍以上
         for (int i = 0; i < nums.length; ++i) {
             if (i != maxIndex && nums[maxIndex] < 2 * nums[i]) {
