d32415543d7495370244b36c0483c3bed5dd647c
Genpeng Xu <xgp1227@gmai.com>
2020-02-06 19:53:20 +0800

    Add the solution of no. 343 problem in the LeetCode

---
+++ src/dp/lc0343_integer_break/Solution1.java
@@ -0,0 +1,32 @@
+package dp.lc0343_integer_break;
+
+/**
+ * This is the solution of No. 343 problem in the LeetCode,
+ * the website of the problem is as follow:
+ * https://leetcode.com/problems/integer-break/
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * Given a positive integer n, break it into the sum of at least two positive integers and maximize
+ * the product of those integers. Return the maximum product you can get.
+ *
+ * Example 1:
+ * Input: 2
+ * Output: 1
+ * Explanation: 2 = 1 + 1, 1 × 1 = 1.
+ *
+ * Example 2:
+ * Input: 10
+ * Output: 36
+ * Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+ * Note: You may assume that n is not less than 2 and not larger than 58.
+ * ==========================================================================================================
+ *
+ * @author  StrongXGP (xgp1227@gmail.com)
+ * @date    2019/07/15
+ */
+public class Solution1 {
+    public int integerBreak(int n) {
+        return 1;
+    }
+}
