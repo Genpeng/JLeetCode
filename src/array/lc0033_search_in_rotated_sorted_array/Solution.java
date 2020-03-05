bbd5d9ae10a50e689b53413c0ff2665c2104aaf8
Genpeng Xu <xgp1227@gmai.com>
2020-03-05 14:05:28 +0800

    modify the solution of no. 33 problem

--- src/array/lc0033_search_in_rotated_sorted_array/Solution.java
+++ src/array/lc0033_search_in_rotated_sorted_array/Solution.java
@@ -33,11 +33,45 @@ public class Solution {
      * 解法：Binary Search
      * 时间复杂度：O(log(N))
      * 空间复杂度：O(1)
+     * <p>
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Search in Rotated Sorted Array.
+     * Memory Usage: 38.4 MB, less than 44.65% of Java online submissions for Search in Rotated Sorted Array.
      *
-     * 备注：
-     * 1. 数组中元素不重复
-     * 2. 数组中元素非负或者不包含-1
-     *
+     * @param nums int[], the input array
+     * @param target int, the integer to search
+     * @return int, the index of target integer
+     */
+    public int searchV1(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        // Step 1: find out the index of minimum integer, O(log(N))
+        int minIdx = findMinIdx(nums);
+        // Step 2: search target by using binary search
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int realMid = (mid + minIdx) % n;
+            if (nums[realMid] < target) {
+                l = mid + 1;
+            } else if (nums[realMid] > target) {
+                r = mid - 1;
+            } else {
+                return realMid;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 解法：Binary Search
+     * 时间复杂度：O(log(N))
+     * 空间复杂度：O(1)
+     * <p>
      * Runtime: 0 ms, faster than 100.00% of Java online submissions for Search in Rotated Sorted Array.
      * Memory Usage: 38.4 MB, less than 44.65% of Java online submissions for Search in Rotated Sorted Array.
      *
@@ -57,16 +91,33 @@ public class Solution {
         }
         int n = nums.length;
         int l, r;
-        if (nums[n-1] == target) {
-            return n-1;
-        } else if (nums[n-1] < target) {
+        if (nums[n - 1] == target) {
+            return n - 1;
+        } else if (nums[n - 1] < target) {
             l = 0;
             r = minIdx - 1;
         } else {
             l = minIdx + 1;
             r = n - 1;
         }
-        // Step 3: find out the target index, O(log(N))
+        // Step 3: find out the target index by using binary search, O(log(N))
+        return binarySearch(nums, l, r, target);
+    }
+
+    private int findMinIdx(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < nums[r]) {
+                r = mid; // 小心这里不能为 `r = mid - 1`，反例比如：[5, 1, 3]
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+    public int binarySearch(int[] nums, int l, int r, int target) {
         while (l <= r) {
             int mid = l + (r - l) / 2;
             if (nums[mid] == target) {
@@ -80,23 +131,16 @@ public class Solution {
         return -1;
     }
 
-    public int findMinIdx(int[] nums) {
-        int l = 0, r = nums.length - 1;
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] < nums[r]) {
-                r = mid;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return l;
-    }
-
     public static void main(String[] args) {
-        int[] nums = new int[] {4, 5, 6, 7, 0, 1, 2}; // the index of minimum is: 4
-        int target = 0;
+        int[] nums1 = new int[]{4, 5, 6, 7, 0, 1, 2};
+        int[] nums2 = new int[]{5, 1, 3};
+        int target = 5;
         Solution solution = new Solution();
-        System.out.println(solution.search(nums, target));
+        // test `findMinIdx(...)` function
+        System.out.println(solution.findMinIdx(nums1));
+        System.out.println(solution.findMinIdx(nums2));
+        // test `search(...)` function
+//        System.out.println(solution.searchV1(nums1, target));
+//        System.out.println(solution.searchV1(nums2, target));
     }
 }
