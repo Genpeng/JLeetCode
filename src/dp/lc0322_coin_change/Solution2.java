684b93706c7b62e9b9a4f1c9a2f4708c555587e9
Genpeng Xu <xgp1227@gmai.com>
2020-03-17 08:56:27 +0800

    add the solution of no. 322 problem

--- src/dp/lc0322_coin_change/Solution2.java
+++ src/dp/lc0322_coin_change/Solution2.java
@@ -1,5 +1,7 @@
 package dp.lc0322_coin_change;
 
+import java.util.Arrays;
+
 /**
  * This is the solution of No. 322 problem in the LeetCode,
  * the website of the problem is as follow:
@@ -26,11 +28,13 @@ package dp.lc0322_coin_change;
  *
  * @author Genpeng Xu (xgp1227atgmail.com)
  */
-public class Solution1 {
+public class Solution2 {
+    private int ans;
+
     /**
-     * Approach 1: Backtracking
-     * Time Complexity: O(S ^ n)
-     * Space Complexity: O(n)
+     * Approach 2: Greedy + DFS
+     * Time Complexity: xxx
+     * Space Complexity: xxx
      * where S is the amount to change, and n is the number of coins
      *
      * @param coins int[], coins of different denominations
@@ -39,42 +43,51 @@ public class Solution1 {
      *              does not exist any combination of coins
      */
     public int coinChange(int[] coins, int amount) {
-        return coinChange(coins, 0, amount);
-    }
-
-    /**
-     * Compute the fewest number of coins that you need to make up that amount, and return -1 if it
-     * does not exist any combination of coins, where the index of coins you can use start from `si`.
-     *
-     * Time Limit Exceeded
-     *
-     * @param coins int[], coins of different denominations
-     * @param si int, the start index of `coins`
-     * @param amount int, target amount of money
-     * @return int, the fewest number of coins that you need to make up that amount, and return -1 if it
-     *              does not exist any combination of coins
-     */
-    private int coinChange(int[] coins, int si, int amount) {
+        if (amount < 0) {
+            return -1;
+        }
         if (amount == 0) {
             return 0;
         }
-        if (si < coins.length) {
-            int minNum = Integer.MAX_VALUE;
-            for (int i = 0; i <= amount / coins[si]; ++i) {
-                int sub = coinChange(coins, si + 1, amount - coins[si] * i);
-                if (sub != -1) {
-                    minNum = Math.min(minNum, i + sub);
-                }
-            }
-            return minNum == Integer.MAX_VALUE ? -1 : minNum;
+        if (coins.length == 0) {
+            return -1;
         }
-        return -1;
+        Arrays.sort(coins);
+        ans = Integer.MAX_VALUE;
+        coinChange(coins, amount, coins.length - 1, 0);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    /**
+     *
+     *
+     * @param coins
+     * @param amount
+     * @param ei
+     * @param count
+     */
+    private void coinChange(int[] coins, int amount, int ei, int count) {
+        if (ei < 0) {
+            return;
+        }
+        for (int k = amount / coins[ei]; k >= 0; --k) {
+            int remain = amount - k * coins[ei];
+            int newCount = count + k;
+            if (remain == 0) {
+                ans = Math.min(ans, newCount);
+                return;
+            }
+            if (newCount + 1 >= ans) {
+                return;
+            }
+            coinChange(coins, remain, ei - 1, newCount);
+        }
     }
 
     public static void main(String[] args) {
         int[] coins = new int[] {1, 2, 5};
         int amount = 11;
-        Solution1 solution = new Solution1();
+        Solution2 solution = new Solution2();
         System.out.println(solution.coinChange(coins, amount));
     }
 }
