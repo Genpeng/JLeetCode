d7c8424311b417de81f816a124a3dc604f6706b2
Genpeng Xu <xgp1227@gmai.com>
2020-04-08 10:03:06 +0800

    add the solution of no. xxxx problem

---
+++ src/c01_array/lcxxxx_counting_elements/Solution1.java
@@ -0,0 +1,90 @@
+package c01_array.lcxxxx_counting_elements;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This is the solution of No. xxx problem in the LeetCode,
+ * the website of the problem is as follow:
+ * xxx
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * Given an integer array arr, count element x such that x + 1 is also in arr.
+ *
+ * If there're duplicates in arr, count them separately.
+ *
+ * Example 1:
+ * Input: arr = [1,2,3]
+ * Output: 2
+ * Explanation: 1 and 2 are counted cause 2 and 3 are in arr.
+ *
+ * Example 2:
+ * Input: arr = [1,1,3,3,5,5,7,7]
+ * Output: 0
+ * Explanation: No numbers are counted, cause there's no 2, 4, 6, or 8 in arr.
+ *
+ * Example 3:
+ * Input: arr = [1,3,2,3,5,0]
+ * Output: 3
+ * Explanation: 0, 1 and 2 are counted cause 1, 2 and 3 are in arr.
+ *
+ * Example 4:
+ * Input: arr = [1,1,2,2]
+ * Output: 2
+ * Explanation: Two 1s are counted cause 2 is in arr.
+ *
+ * Constraints:
+ * - 1 <= arr.length <= 1000
+ * - 0 <= arr[i] <= 1000
+ * ==========================================================================================================
+ *
+ * Tags: array;hash table;
+ *
+ * @author  Genpeng Xu (xgp1227atgmail.com)
+ */
+public class Solution1 {
+    /**
+     * Approach 1: Hash Table
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     *
+     * @param arr int[], the input integer array
+     * @return int, the number of elements `x` whose `x+1` also in `arr`
+     */
+    public int countElementsV1(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+        int ans = 0;
+        for (int num : arr) {
+            if (set.contains(num+1)) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * Approach 1: Hash Table
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     *
+     * @param arr int[], the input integer array
+     * @return int, the number of elements `x` whose `x+1` also in `arr`
+     */
+    public int countElementsV2(int[] arr) {
+        int[] counts = new int[1002];
+        for (int num : arr) {
+            ++counts[num];
+        }
+        int ans = 0;
+        for (int num : arr) {
+            if (counts[num+1] > 0) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
