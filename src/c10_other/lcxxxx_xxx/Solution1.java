56de2e7c4b4129d381d0fbce697b6c4d7f5b28f8
Genpeng Xu <xgp1227@gmai.com>
2020-04-27 14:26:20 +0800

    modify the project structure

---
+++ src/c10_other/lcxxxx_xxx/Solution1.java
@@ -0,0 +1,19 @@
+package c10_other.lcxxxx_xxx;
+
+/**
+ * This is the solution of No. xxx problem in the LeetCode,
+ * the website of the problem is as follow:
+ * xxx
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * xxx
+ * ==========================================================================================================
+ *
+ * Tags: xxx;xxx
+ *
+ * @author  Genpeng Xu (xgp1227atgmail.com)
+ */
+public class Solution1 {
+
+}
