bc88ee30a4b1b892008c9da56e9341365e9b181d
Genpeng Xu <xgp1227@gmai.com>
2020-05-26 08:50:35 +0800

    add the solution of no. 344 problem

---
+++ src/c02_string/lc0344_reverse_string/Solution.java
@@ -0,0 +1,38 @@
+package c02_string.lc0344_reverse_string;
+
+/**
+ * This is the solution of No. 344 problem in the LeetCode,
+ * the website of the problem is as follow:
+ * https://leetcode.com/problems/reverse-string/
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * Write a function that reverses a string. The input string is given as an array of characters char[].
+ *
+ * Do not allocate extra space for another array, you must do this by
+ * modifying the input array in-place with O(1) extra memory.
+ *
+ * You may assume all the characters consist of printable ascii characters.
+ *
+ * Example 1:
+ * Input: ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ *
+ * Example 2:
+ * Input: ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ * ==========================================================================================================
+ *
+ * Tags: string;two pointers;
+ *
+ * @author  Genpeng Xu (xgp1227atgmail.com)
+ */
+public class Solution {
+    public void reverseString(char[] s) {
+        for (int i = 0, j = s.length - 1; i < j; ++i, --j) {
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+        }
+    }
+}
