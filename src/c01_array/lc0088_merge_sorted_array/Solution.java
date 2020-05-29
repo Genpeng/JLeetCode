94ff6823b6794f45a4b7d802b582d1f0287def72
Genpeng Xu <xgp1227@gmai.com>
2020-05-29 19:32:45 +0800

    modify the solution of no. 88 problem

--- src/c01_array/lc0088_merge_sorted_array/Solution.java
+++ src/c01_array/lc0088_merge_sorted_array/Solution.java
@@ -23,6 +23,8 @@ import java.util.Arrays;
  * Output: [1,2,2,3,5,6]
  * ==========================================================================================================
  *
+ * Tags: array;two pointers;
+ *
  * @author  StrongXGP (xgp1227@gmail.com)
  * @date    2019/06/28
  */
@@ -30,19 +32,14 @@ public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1, j = n - 1, k = m + n - 1;
         while (i >= 0 && j >= 0) {
-            if (nums2[j] > nums1[i]) {
-                nums1[k] = nums2[j];
-                --j;
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
             } else {
-                nums1[k] = nums1[i];
-                --i;
+                nums1[k--] = nums2[j--];
             }
-            --k;
         }
         while (j >= 0) {
-            nums1[k] = nums2[j];
-            --j;
-            --k;
+            nums1[k--] = nums2[j--];
         }
     }
 
