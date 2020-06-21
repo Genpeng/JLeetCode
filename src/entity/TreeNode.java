002102d5ea0586fde86789cd7172b579471dd398
Genpeng Xu <xgp1227@gmai.com>
2020-06-21 11:32:54 +0800

    modify the TreeNode entity

--- src/entity/TreeNode.java
+++ src/entity/TreeNode.java
@@ -5,7 +5,17 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
+    public TreeNode() {
+
+    }
+
     public TreeNode(int val) {
+        this(val, null, null);
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
