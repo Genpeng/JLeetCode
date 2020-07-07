1858fe5df52200bf3cd2f090a934ca74515f8373
Genpeng Xu <xgp1227@gmai.com>
2020-07-07 15:54:11 +0800

    modify the solution of no. 347 problem

--- src/c06_heap_and_priority_queue/lc0347_top_k_frequent_element/Solution1.java
+++ src/c06_heap_and_priority_queue/lc0347_top_k_frequent_element/Solution1.java
@@ -1,4 +1,4 @@
-package c06_heap_and_priority_queue.leetcode347;
+package c06_heap_and_priority_queue.lc0347_top_k_frequent_element;
 
 import java.util.*;
 
@@ -11,7 +11,7 @@ import java.util.*;
  * @author  StrongXGP (xgp1227@gmail.com)
  * @date    2018/11/11
  */
-public class Solution2 {
+public class Solution1 {
     public List<Integer> topKFrequent(int[] nums, int k) {
         // 统计元素的频率
         Map<Integer, Integer> map = new HashMap<>(16);
@@ -64,7 +64,7 @@ public class Solution2 {
     public static void main(String[] args) {
         int[] nums = {1, 1, 1, 2, 2, 3};
         int k = 2;
-        Solution2 solution = new Solution2();
+        Solution1 solution = new Solution1();
         printList(solution.topKFrequent(nums, k));
     }
 }
