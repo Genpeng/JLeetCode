851b817064b1f7aa52c97a4fdd85004e4da37c9f
Genpeng Xu <xgp1227@gmai.com>
2020-07-07 10:59:17 +0800

    modify the solution of no. 121 problem

--- src/c08_dp/lc0121_best_time_to_buy_and_sell_stock/Solution2.java
+++ src/c08_dp/lc0121_best_time_to_buy_and_sell_stock/Solution2.java
@@ -46,11 +46,11 @@ public class Solution2 {
      */
     public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE, maxProfit = 0;
-        for (int i = 0; i < prices.length; ++i) {
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            } else if (prices[i] - minPrice > maxProfit) {
-                maxProfit = prices[i] - minPrice;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
             }
         }
         return maxProfit;
