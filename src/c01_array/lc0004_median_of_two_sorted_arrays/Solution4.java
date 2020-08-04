0d36574f2390494b85711a0be98cd2e16d79e80d
Genpeng Xu <xgp1227@gmai.com>
2020-08-04 10:32:23 +0800

    modify the solution of no. 4 problem

--- src/c01_array/lc0004_median_of_two_sorted_arrays/Solution4.java
+++ src/c01_array/lc0004_median_of_two_sorted_arrays/Solution4.java
@@ -31,17 +31,49 @@ package c01_array.lc0004_median_of_two_sorted_arrays;
  */
 public class Solution4 {
     /**
-     * Approach 4:
-     * Time Complexity:
-     * Space Complexity:
+     * Approach 4: Median's property
+     *
+     * References:
+     * - https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xun-zhao-liang-ge-you-xu-shu-zu-de-zhong-wei-s-114/
+     *
+     * Complexity Analysis:
+     * Time Complexity: O(log(min{m, n}))
+     * Space Complexity: O(1)
      *
      * @param A int[], one integer array
      * @param B int[], the other integer array
      * @return int[], the median of two sorted array
      */
     public double findMedianSortedArrays(int[] A, int[] B) {
-        // TODO: add it
-        return -1;
+        if (A == null || B == null) {
+            throw new IllegalArgumentException("[ERROR] There must exist null array in two input arrays!!!");
+        }
+        int m = A.length, n = B.length;
+        if (m + n == 0) {
+            throw new IllegalArgumentException("[ERROR] The two input arrays are both empty!!!");
+        }
+        if (m > n) {
+            return findMedianSortedArrays(B, A);
+        }
+        int L = m + n, half = (L + 1) >> 1;
+        int aLeft = 0, aRight = m;
+        int median1 = 0, median2 = 0;
+        while (aLeft <= aRight) {
+            int i = (aLeft + aRight) >> 1;
+            int j = half - i;
+            int L1 = i == 0 ? Integer.MIN_VALUE : A[i-1];
+            int R1 = i == m ? Integer.MAX_VALUE : A[i];
+            int L2 = j == 0 ? Integer.MIN_VALUE : B[j-1];
+            int R2 = j == n ? Integer.MAX_VALUE : B[j];
+            if (L1 <= R2) {
+                median1 = Math.max(L1, L2);
+                median2 = Math.min(R1, R2);
+                aLeft = i + 1;
+            } else {
+                aRight = i - 1;
+            }
+        }
+        return (L & 1) == 0 ? (median1 + median2) * 0.5 : median1 * 1.0;
     }
 
     public static void main(String[] args) {
