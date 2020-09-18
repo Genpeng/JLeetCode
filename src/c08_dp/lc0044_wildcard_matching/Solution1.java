95a331dcc624101508e10869cb10f70f89ff9025
Genpeng Xu <xgp1227@gmai.com>
2020-09-18 14:48:44 +0800

    modify the solution of no. 44 problem

--- src/c08_dp/lc0044_wildcard_matching/Solution1.java
+++ src/c08_dp/lc0044_wildcard_matching/Solution1.java
@@ -81,18 +81,18 @@ public class Solution1 {
         int m = s.length(), n = p.length();
         boolean[][] dp = new boolean[m+1][n+1];
         dp[0][0] = true;
-        for (int i = 1; i <= n; ++i) {
-            if (p.charAt(i-1) != '*') {
+        for (int j = 1; j <= n; ++j) {
+            if (p.charAt(j-1) != '*') {
                 break;
             }
-            dp[0][i] = true;
+            dp[0][j] = true;
         }
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 if (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?') {
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                } else if (p.charAt(j-1) == '*') {
                     dp[i][j] = dp[i-1][j-1];
+                } else if (p.charAt(j-1) == '*') {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
                 }
             }
         }
