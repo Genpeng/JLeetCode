72e8235c7150687b2856c711d64e304deec8e193
Genpeng Xu <xgp1227@gmai.com>
2020-12-24 11:32:56 +0800

    add the solution of no. 93 problem

---
+++ src/c02_string/lc0093_restore_ip_addresses/Solution1.java
@@ -0,0 +1,111 @@
+package c02_string.lc0093_restore_ip_addresses;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * This is the solution of No. 93 problem in the LeetCode,
+ * the website of the problem is as follow:
+ * https://leetcode.com/problems/restore-ip-addresses/
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * Given a string s containing only digits, return all possible valid IP addresses that can be obtained
+ * from s. You can return them in any order.
+ *
+ * A valid IP address consists of exactly four integers, each integer is between 0 and 255, separated by
+ * single dots and cannot have leading zeros. For example, "0.1.2.201" and "192.168.1.1" are valid IP addresses
+ * and "0.011.255.245", "192.168.1.312" and "192.168@1.1" are invalid IP addresses.
+ *
+ * Example 1:
+ * Input: s = "25525511135"
+ * Output: ["255.255.11.135","255.255.111.35"]
+ *
+ * Example 2:
+ * Input: s = "0000"
+ * Output: ["0.0.0.0"]
+ *
+ * Example 3:
+ * Input: s = "1111"
+ * Output: ["1.1.1.1"]
+ *
+ * Example 4:
+ * Input: s = "010010"
+ * Output: ["0.10.0.10","0.100.1.0"]
+ *
+ * Example 5:
+ * Input: s = "101023"
+ * Output: ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
+ *
+ * Constraints:
+ * - 0 <= s.length <= 3000
+ * - s consists of digits only.
+ * ==========================================================================================================
+ *
+ * Difficulty: Medium
+ * Tags: string;backtracking;
+ *
+ * @author Genpeng Xu (xgp1227atgmail.com)
+ */
+public class Solution1 {
+    /**
+     * Approach 1: Backtracking with pruning
+     *
+     * Reference:
+     * [1] Notability 上这道题的树形图
+     *
+     * Result of Submission:
+     * Runtime: 1 ms, faster than 99.56% of Java online submissions for Restore IP Addresses.
+     * Memory Usage: 37.5 MB, less than 99.05% of Java online submissions for Restore IP Addresses.
+     *
+     * @param s String, a string s containing only digits
+     * @return List<String>, all possible valid IP addresses
+     */
+    public List<String> restoreIpAddresses(String s) {
+        final int L = s.length();
+        List<String> ips = new LinkedList<>();
+        if (L < 4 || L > 12) {
+            return ips;
+        }
+        int[] path = new int[4];
+        dfs(s, 4, L, 0, path, 0, ips);
+        return ips;
+    }
+
+    private void dfs(String s, int remainNum, int remainLen, int start, int[] path, int idx, List<String> ips) {
+        if (remainNum == 0) {
+            if (remainLen == 0) {
+                StringBuilder ip = new StringBuilder();
+                for (int num : path) {
+                    ip.append(num).append('.');
+                }
+                ip.deleteCharAt(ip.length() - 1);
+                ips.add(ip.toString());
+            }
+            // remainNum == 0 && remainLen > 0，剪枝条件3
+            return;
+        }
+        if (s.charAt(start) == '0') {
+            path[idx] = 0;
+            dfs(s, remainNum - 1, remainLen - 1, start+1, path, idx+1, ips);
+            return;
+        }
+        int currNum = 0;
+        for (int i = 1; i < 4 && start + i <= s.length(); ++i) {
+            currNum = currNum * 10 + (s.charAt(start+i-1) - '0');
+            if (remainLen-i < remainNum-1 || remainLen-i > (remainNum-1) * 3) { // 剪枝条件1
+                continue;
+            }
+            if (currNum > 255) { // 剪枝条件2
+                continue;
+            }
+            path[idx] = currNum;
+            dfs(s, remainNum-1, remainLen-i, start+i, path, idx+1, ips);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution1 solu = new Solution1();
+        System.out.println(solu.restoreIpAddresses("0000"));
+    }
+}
