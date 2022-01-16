4ffc1b8eb941887a80ec3c42de56df53a295ae6e
Lukas(许根鹏)[运营中心] <gerry.xu@c02gx3kcq05p.lan>
2022-01-16 23:21:58 +0800

    :memo: test commit

--- src/c01_array/lc0001_two_sum/Solution1.java
+++ src/c01_array/lc0001_two_sum/Solution1.java
@@ -6,35 +6,35 @@ import util.PrintUtil;
  * This is the solution of No. 1 problem in the LeetCode,
  * the website of the problem is as follow:
  * https://leetcode.com/problems/two-sum/
- *
+ * <p>
  * The description of problem is as follow:
  * ==========================================================================================================
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- *
+ * <p>
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- *
+ * <p>
  * Example:
- *
+ * <p>
  * Given nums = [2, 7, 11, 15], target = 9,
- *
+ * <p>
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
  * ==========================================================================================================
- *
+ * <p>
  * Difficulty: Easy
  * Tags: array;hash table;
  *
- * @author  Genpeng Xu (xgp1227atgmail.com)
+ * @author Genpeng Xu (xgp1227atgmail.com)
  */
 public class Solution1 {
     /**
      * Approach 1: Brute Force
-     *
+     * <p>
      * Complexity Analysis:
      * Time Complexity: O(N ^ 2)
      * Space Complexity: O(1)
      *
-     * @param nums int[], the input integer array
+     * @param nums   int[], the input integer array
      * @param target int, the target integer
      * @return int[], indices of the two numbers such that they add up to a specific target
      */
