89cd6870a8ae0f2e80a0ed9388402b6e18f0091f
Lukas <xgp1227@gmail.com>
2023-03-08 19:36:32 +0800

    update solution of no. 2 problem add two numbers

--- src/c03_linked_list/lc0002_add_two_numbers/Solution1.java
+++ src/c03_linked_list/lc0002_add_two_numbers/Solution1.java
@@ -24,7 +24,7 @@ import entity.ListNode;
  * Difficulty: Medium
  * Tags: linked list;
  *
- * @author  StrongXGP (xgp1227@gmail.com)
+ * @author  Lukas (xgp1227@gmail.com)
  * @date    2019/03/20
  */
 public class Solution1 {
@@ -42,17 +42,45 @@ public class Solution1 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(-1);
         ListNode tail = dummyHead;
+        int s;
         int carry = 0;
         while (l1 != null || l2 != null) {
-            int s = carry;
+            s = carry;
             s += l1 == null ? 0 : l1.val;
             s += l2 == null ? 0 : l2.val;
             tail.next = new ListNode(s % 10);
-            tail = tail.next;
-            carry = s / 10;
             // update to next iteration
+            carry = s / 10;
             l1 = l1 == null ? l1 : l1.next;
             l2 = l2 == null ? l2 : l2.next;
+            tail = tail.next;
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+    public ListNode addTwoNumbersV2(ListNode l1, ListNode l2) {
+        // 输入：两个链表，分别表示两个正整数，需要注意的是，链表存储的是整数的逆序，比如：整数 123，用链表 3->2->1 表示
+        // 输出：两个正整数相加的结果，同样的，链表存储的也是整数的逆序
+
+        ListNode dummyHead = new ListNode(-1);
+        ListNode tail = dummyHead;
+        int s, carry = 0;
+        while (l1 != null || l2 != null) {
+            s = carry;
+            if (l1 != null) {
+                s += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                s += l2.val;
+                l2 = l2.next;
+            }
+            tail.next = new ListNode(s % 10);
+            carry = s / 10;
+            tail = tail.next;
         }
         if (carry > 0) {
             tail.next = new ListNode(carry);
@@ -63,6 +91,6 @@ public class Solution1 {
     public static void main(String[] args) {
         ListNode l1 = new ListNode(new int[] {2, 4, 3});
         ListNode l2 = new ListNode(new int[] {5, 6, 4});
-        System.out.println((new Solution1()).addTwoNumbers(l1, l2));
+        System.out.println((new Solution1()).addTwoNumbersV2(l1, l2));
     }
 }
