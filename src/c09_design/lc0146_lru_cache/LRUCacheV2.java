6ff1535e3a7ec119e739e048605c543950ac2766
Lukas <xgp1227@gmail.com>
2023-05-31 17:09:25 +0800

    [LeetCode] modify solution of no. 146 LRU Cache

--- src/c09_design/lc0146_lru_cache/LRUCacheV2.java
+++ src/c09_design/lc0146_lru_cache/LRUCacheV2.java
@@ -11,11 +11,11 @@ import java.util.Map;
  * The description of problem is as follow:
  * ==========================================================================================================
  * Design and implement a data structure for Least Recently Used (LRU) cache.
- * It should support the following operations: get and put.
  *
+ * It should support the following operations: get and put.
  * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
  * put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity,
- *                   it should invalidate the least recently used item before inserting a new item.
+ * it should invalidate the least recently used item before inserting a new item.
  *
  * The cache is initialized with a positive capacity.
  *
