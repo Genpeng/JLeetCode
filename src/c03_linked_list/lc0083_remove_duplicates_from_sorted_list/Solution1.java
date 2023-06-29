134a18f3e0803d53d0b475e1d4c3529d717e8122
Lukas <xgp1227@gmail.com>
2023-06-29 16:07:06 +0800

    [LeetCode] add solution of no. 83 remove duplicates from sorted list

---
+++ src/c03_linked_list/lc0083_remove_duplicates_from_sorted_list/Solution1.java
@@ -0,0 +1,20 @@
+package c03_linked_list.lc0083_remove_duplicates_from_sorted_list;
+
+/**
+ * This is the solution of No. 83 problem in the LeetCode,
+ * the website of the problem is as follow:
+ * xxx
+ *
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * xxx
+ * ==========================================================================================================
+ *
+ * Difficulty: Easy or Medium or Hard
+ * Tags: xxx;xxx;
+ *
+ * @author Genpeng Xu (xgp1227atgmail.com)
+ */
+public class Solution1 {
+
+}
