646d325cdad38be729cdb44f889899d32b5fd9ab
Genpeng <xgp1227@gmail.com>
2024-08-05 23:36:05 +0800

    :memo: modify solution of no. 438 find all anagrams in a string

--- src/c02_string/lc0438_find_all_anagrams_in_a_string/Solution1.java
+++ src/c02_string/lc0438_find_all_anagrams_in_a_string/Solution1.java
@@ -1,24 +1,21 @@
 package c02_string.lc0438_find_all_anagrams_in_a_string;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * This is the solution of No. 438 problem in the LeetCode,
  * the website of the problem is as follow:
- * https://leetcode.com/problems/find-all-anagrams-in-a-string/
- *
+ * https://leetcode.com/problems/find-all-anagrams-in-a-string
+ * <p>
  * The description of problem is as follow:
  * ==========================================================================================================
  * Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
- *
+ * <p>
  * Strings consists of lowercase English letters only and the length of both strings s and p
  * will not be larger than 20,100.
- *
+ * <p>
  * The order of output does not matter.
- *
+ * <p>
  * Example 1:
  * Input:
  * s: "cbaebabacd" p: "abc"
@@ -27,7 +24,7 @@ import java.util.Map;
  * Explanation:
  * The substring with start index = 0 is "cba", which is an anagram of "abc".
  * The substring with start index = 6 is "bac", which is an anagram of "abc".
- *
+ * <p>
  * Example 2:
  * Input:
  * s: "abab" p: "ab"
@@ -38,7 +35,7 @@ import java.util.Map;
  * The substring with start index = 1 is "ba", which is an anagram of "ab".
  * The substring with start index = 2 is "ab", which is an anagram of "ab".
  * ==========================================================================================================
- *
+ * <p>
  * Difficulty: Medium
  * Tags: string;sliding window;
  *
@@ -47,43 +44,65 @@ import java.util.Map;
 class Solution1 {
     /**
      * Approach 1: Sliding Window (fixed window)
+     * Time Complexity: O(m + n)
+     * Space Complexity: O(1)
      *
      * @param s String, the input string
      * @param p String, the pattern string
      * @return List<Integer>, all the start indices of p's anagrams in s
      */
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> result = new LinkedList<>();
-        // 步骤1：统计 p 中所有字符的频次
-        Map<Character, Integer> freqMap = new HashMap<>(p.length());
-        for (char c : p.toCharArray()) {
-            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        // 题意：
+        // 已知两个字符串 s 和 p，要求找出 s 中所有 p 的异位词（anagram），并返回它们的起始位置
+
+        // 解法1：滑动窗口（固定窗口）
+        // 时间复杂度：O(m + n)
+        // 空间复杂度：O(1)
+
+        List<Integer> ans = new ArrayList<>();
+        int m = s.length(), n = p.length();
+        if (m < n) {
+            return ans;
         }
-        // 步骤2：用一个长度为 p.length() 的窗口向右滑动，当窗口中的子串是 p 的排列时，
-        // 将窗口的左边界加入到结果中
-        int matched = 0;
+
+        // 步骤1：统计字符串 p 中字符的频次
+        Map<Character, Integer> freq = new HashMap<>(26);
         char c;
-        for (int li = 0, ri = 0; ri < s.length(); ++ri) {
+        for (int i = 0; i < n; ++i) {
+            c = p.charAt(i);
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+
+        // 步骤2：用一个长度为 n（字符串 p 的长度）的窗口向右滑动，每次向右滑动一步，
+        // 如果当前窗口是 p 的异位词，则将起始位置加入到结果中
+        int matched = 0;
+        for (int li = 0, ri = 0; ri < m; ++ri) {
             c = s.charAt(ri);
-            if (freqMap.containsKey(c)) {
-                freqMap.put(c, freqMap.get(c) - 1);
-                if (freqMap.get(c) == 0) {
+            if (freq.containsKey(c)) {
+                freq.put(c, freq.get(c) - 1);
+                if (freq.get(c) == 0) {
                     ++matched;
                 }
             }
-            if (matched == freqMap.size()) {
-                result.add(li);
-            }
-            if (ri >= p.length() - 1) {
+            if (ri >= n-1) {
+                if (matched == freq.size()) {
+                    ans.add(li);
+                }
                 c = s.charAt(li++);
-                if (freqMap.containsKey(c)) {
-                    if (freqMap.get(c) == 0) {
+                if (freq.containsKey(c)) {
+                    if (freq.get(c) == 0) {
                         --matched;
                     }
-                    freqMap.put(c, freqMap.get(c) + 1);
+                    freq.put(c, freq.get(c) + 1);
                 }
             }
         }
-        return result;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution1 solu = new Solution1();
+        System.out.println(solu.findAnagrams("cbaebabacd", "abc")); // [0, 6]
+        System.out.println(solu.findAnagrams("abab", "ab")); // [0, 1, 2]
     }
 }
--- src/c02_string/lc0438_find_all_anagrams_in_a_string/Solution2.java
+++ src/c02_string/lc0438_find_all_anagrams_in_a_string/Solution2.java
@@ -1,22 +1,21 @@
 package c02_string.lc0438_find_all_anagrams_in_a_string;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * This is the solution of No. 438 problem in the LeetCode,
  * the website of the problem is as follow:
- * https://leetcode.com/problems/find-all-anagrams-in-a-string/
- *
+ * https://leetcode.com/problems/find-all-anagrams-in-a-string
+ * <p>
  * The description of problem is as follow:
  * ==========================================================================================================
  * Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
- *
+ * <p>
  * Strings consists of lowercase English letters only and the length of both strings s and p
  * will not be larger than 20,100.
- *
+ * <p>
  * The order of output does not matter.
- *
+ * <p>
  * Example 1:
  * Input:
  * s: "cbaebabacd" p: "abc"
@@ -25,7 +24,7 @@ import java.util.List;
  * Explanation:
  * The substring with start index = 0 is "cba", which is an anagram of "abc".
  * The substring with start index = 6 is "bac", which is an anagram of "abc".
- *
+ * <p>
  * Example 2:
  * Input:
  * s: "abab" p: "ab"
@@ -36,7 +35,7 @@ import java.util.List;
  * The substring with start index = 1 is "ba", which is an anagram of "ab".
  * The substring with start index = 2 is "ab", which is an anagram of "ab".
  * ==========================================================================================================
- *
+ * <p>
  * Difficulty: Medium
  * Tags: string;sliding window;
  *
@@ -51,34 +50,48 @@ class Solution2 {
      * @return List<Integer>, all the start indices of p's anagrams in s
      */
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> result = new LinkedList<>();
+        // 题意：
+        // 已知两个字符串 s 和 p，要求找出 s 中所有 p 的异位词（anagram），并返回它们的起始位置
+
+        // 解法2：滑动窗口（可变窗口）
+        // 时间复杂度：O(m + n)
+        // 空间复杂度：O(1)
+
+        List<Integer> ans = new ArrayList<>();
         int m = s.length(), n = p.length();
         if (m < n) {
-            return result;
+            return ans;
         }
         // 步骤1：统计 p 中所有字符的出现频次
-        int[] freqMap = new int[128];
-        for (char c : p.toCharArray()) {
-            ++freqMap[c];
+        int[] freq = new int[128];
+        for (int i = 0; i < n; ++i) {
+            ++freq[p.charAt(i)];
         }
-        // 步骤2：用一个大小可变的窗口向右滑动
+        // 步骤2：维护一个大小可变的窗口，一开始先尽可能向右滑动，直到包含字符串 p 的异位词（anagram），
+        // 然后开始收缩（左边界），收缩时，判断当前窗口是否为 p 的异位词，如果是的话，加入结果中
         int count = n;
         char c;
         for (int li = 0, ri = 0; ri < m; ++ri) {
             c = s.charAt(ri);
-            if (freqMap[c]-- > 0) {
+            if (freq[c]-- > 0) {
                 --count;
             }
             while (count == 0) {
                 if (ri - li + 1 == n) {
-                    result.add(li);
+                    ans.add(li);
                 }
                 c = s.charAt(li++);
-                if (++freqMap[c] > 0) {
+                if (freq[c]++ == 0) {
                     ++count;
                 }
             }
         }
-        return result;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution2 solu = new Solution2();
+        System.out.println(solu.findAnagrams("cbaebabacd", "abc")); // [0, 6]
+        System.out.println(solu.findAnagrams("abab", "ab")); // [0, 1, 2]
     }
 }
