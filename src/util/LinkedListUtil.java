160a982d50173b94b311cad2aaae6eca624d6803
Genpeng <xgp1227@gmail.com>
2024-08-13 23:58:15 +0800

    :memo: update linked list util

--- src/util/LinkedListUtil.java
+++ src/util/LinkedListUtil.java
@@ -26,4 +26,41 @@ public class LinkedListUtil {
         }
         return slow;
     }
+
+    /**
+     * Cut linked list into two parts
+     *
+     * @param head ListNode, head of linked list
+     * @return ListNode[], a pair of two elements, where the first element is the end of the first half
+     *         and the second element is the beginning of the second half
+     */
+    public static ListNode[] bisectV2(ListNode head) {
+        ListNode prev = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (prev != null) {
+            prev.next = null;
+        }
+        return new ListNode[] {prev, slow};
+    }
+
+    /**
+     * Reverse linked list
+     *
+     * @param head ListNode, the head of linked list
+     * @return ListNode, the reversed linked list
+     */
+    public static ListNode reverse(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
 }
